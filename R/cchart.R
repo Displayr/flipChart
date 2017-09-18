e2af8f858cb8cbabd09e0572b00150b46ad53f49
TimBock <tim.bock@numbers.net.au>
2017-09-18 17:17:09 +1000

    DS-1514: Requiring chart function name only as the first argument

--- R/cchart.R
+++ R/cchart.R
@@ -1,7 +1,7 @@
 #' CChart
 #'
 #' Creates charts
-#' @param chart.function The name of the function, or the function itself, used for creating the chart (aka plot).
+#' @param chart.function.name The name of the function used for creating the chart (aka plot).
 #' @param x The data to be plotted.
 #' @param ... Arguments to the function \code{chart.type}
 #' @param warn.if.no.match If TRUE, a warning is shown if any arugments are not matched.
@@ -11,9 +11,8 @@
 #' @return A chart object that can be printed. Most often, a plotly object.
 #' @export
 
-CChart <- function(chart.function, x,  ..., warn.if.no.match = TRUE)
+CChart <- function(chart.function.name, x,  ..., warn.if.no.match = TRUE)
 {
-    chart.function.name <- deparse(substitute(chart.function)) # converting to a character
     fun.and.pars <- getFunctionAndParameters(chart.function.name)
     arguments <- substituteArgumentNames(fun.and.pars$parameters.o, list(...), warn.if.no.match)
     args <- paste0("c(list(", fun.and.pars$parameter.1, " = x), arguments)")
