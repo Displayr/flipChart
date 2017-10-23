9c6d266fe4c40de267ad55c56d5c80aaaeee1028
TimBock <tim.bock@numbers.net.au>
2017-10-23 18:35:50 +1100

    DS-1480 Various tidying
    1. Dealing with removing *Chart* from the functionnames
    2. Removing substituteAxisName
    3.  Adding unit tests
    
    THis commit includes some failued unit tests in test-preparedata(305,
    310) and test-prepdataremoval Removing Raws and Columns (259), which are
    hopefully machine-specific.

---
+++ tests/test-cchart-uglystandardcharts.R
@@ -0,0 +1,22 @@
+context("Test 'ugly' standard charts via CChart")
+# The ones that don't depend on Distribution are in the testthat directory
+library(flipStandardCharts)
+
+
+test_that("Violin", {
+    set.seed(1223)
+    z = list(Normal = rnorm(1000), "Poisson with unit lamda" = rpois(1000, 1), Exponential = rexp(1000))
+    Violin(z, title = "Comparing distributions",
+           values.title = "Values",
+           global.font.family = "Courier",
+           global.font.color = "Red")
+    CChart("Violin", z, title = "Comparing distributions",
+           values.title = "Values",
+           global.font.family = "Courier",
+           global.font.color = "Red")
+    CChart("Violin", z, title = "Comparing distributions",
+           y.title = "Values",
+           global.font.family = "Courier",
+           global.font.color = "Red")
+
+})
