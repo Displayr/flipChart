b9efe73202df2bebeda2abe6eff9c42ca8f10b65
jakehoare <jake_hoare@hotmail.com>
2017-12-11 17:04:35 +1100

    DS-1715 Supress number of colors warning for Time Series since with range only one color required for 3 series

--- R/preparecolors.R
+++ R/preparecolors.R
@@ -46,7 +46,7 @@ PrepareColors <- function(dat, chart.type, scatter.colors.column = 4,
                                                       custom.gradient.start = palette.custom.gradient.start,
                                                       custom.gradient.end = palette.custom.gradient.end,
                                                       custom.palette = palette.custom.palette,
-                                                      silent = chart.type == "Bar Pictograph")
+                                                      silent = chart.type %in% c("Bar Pictograph", "Time Series"))
 
     if (!is.null(fit.palette) && fit.palette != "Group colors")
         fit.line.colors <- ChartColors(num.colors[[1]], given.colors = fit.palette,
