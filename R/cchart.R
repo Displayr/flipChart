0b16eddef5f5a282a2c6619e2a25b39bda39cb43
chschan <chschan@users.noreply.github.com>
2022-02-22 23:30:50 +1100

    Fix handling of percentage data

--- R/cchart.R
+++ R/cchart.R
@@ -680,6 +680,10 @@ setScatterAxesBounds <- function(settings, data)
     ind.x <- v.ind["x"]
     ind.y <- v.ind["y"]
 
+    if (is.numeric(data) && !is.null(attr(data, "statistic")) &&
+        grepl(attr(data, "statistic"), "%"))
+        data <- data/100
+
     if (is.null(settings$ValueAxis$Minimum) && .isValidIndex(ind.y))
     {
         rg <- range(data[,ind.y], na.rm = TRUE)
@@ -990,10 +994,6 @@ convertChartDataToNumeric <- function(data)
 {
     .isValidIndex <- function(i) {return (!is.null(i) && !is.na(i) && i > 0 &&
                         i <= NCOL(data))}
-
-    if (is.numeric(data) && !is.null(attr(data, "statistic")) &&
-        grepl(attr(data, "statistic"), "%"))
-        return(data/100)
 
     v.ind <- attr(data, "scatter.variable.indices")
     new.data <- suppressWarnings(AsNumeric(data, binary = FALSE))
