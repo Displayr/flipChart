1af69cc8642c9f1a01e0df6b2e975a72a7348627
alexhartstone <alex.hartstone@displayr.com>
2022-03-01 15:15:11 +1100

    RS-9977: rename attribute [revdep skip]

--- R/cchart.R
+++ R/cchart.R
@@ -308,7 +308,7 @@ CChart <- function(chart.type, x, small.multiples = FALSE,
         attr(result,  "ChartData") <- x # Used by Displayr to permit exporting of the raw data.
     class(result) <- c(class(result), "visualization-selector")
     attr(result,  "ChartSettings") <- chart.settings
-    attr(result, "footer.html") <- attr(x, "footer.html")
+    attr(result, "footerhtml") <- attr(x, "footerhtml")
     result
 }
 
