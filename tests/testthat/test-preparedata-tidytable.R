06172562b0ed9e65de334a70c5c3eec8836a566c
chschan <chschan@users.noreply.github.com>
2023-03-14 18:10:15 +1100

    Add more print statements

--- tests/testthat/test-preparedata-tidytable.R
+++ tests/testthat/test-preparedata-tidytable.R
@@ -283,6 +283,7 @@ test_that("Time series object",
     res <- PrepareData("Column", input.data.table = EuStockMarkets, sort.columns = TRUE,
                 sort.columns.row = 1860, first.k.columns = 1, tidy = FALSE)
     print(EuStockMarkets[1860,])
+    dput(res$data[1860])
     #expect_equal(colnames(res$data), "CAC") # For some reason travis CI loses the colnames
     expect_equal(res$data[1860], 3995)
 })
