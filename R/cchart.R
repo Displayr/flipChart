c51d9a65d28cfd8b1eb419ae13b9a70b3b8bbed4
chschan <chschan@users.noreply.github.com>
2024-06-24 16:58:41 +1000

    FS2-2490: Support exporting to editable chart

--- R/cchart.R
+++ R/cchart.R
@@ -348,7 +348,7 @@ CChart <- function(chart.type, x, small.multiples = FALSE,
     chart.settings <- updateLabels(chart.settings, attr(result, "ChartLabels"))
 
     # Convert data after the charting function has been applied
-    if (chart.type %in% c("Scatter", "Bubble", "CombinedScatter"))
+    if (isScatter(chart.type))
     {
         #result <- addScatterAxisWarning(result, x) # set warning before data conversion
         x <- convertChartDataToNumeric(x)
@@ -436,7 +436,7 @@ addLabels <- function(x, chart.type, chart.title, categories.title, values.title
         chart.labels$PrimaryAxisTitle <- categories.title
     if (any(nzchar(values.title)))
         chart.labels$ValueAxisTitle <- values.title
-    if (!chart.type %in% c("Scatter", "Bubble") && any(nzchar(data.label.format)))
+    if (!isScatter(chart.type) && any(nzchar(data.label.format)))
     {
         numformat <- convertToPPTNumFormat(data.label.format)
         if (!is.null(numformat) && length(chart.labels$SeriesLabels) > 0)
@@ -531,7 +531,7 @@ getPPTSettings <- function(chart.type, args, data)
     {
         if (chart.type %in% c("Area", "Radar", "Palm") && !tmp.is.stacked)
             tmp.opacity <- 0.4
-        else if (chart.type == "Scatter" && isTRUE(attr(data, "scatter.variable.indices")["sizes"] <= NCOL(data)))
+        else if (isScatter(chart.type) && isTRUE(attr(data, "scatter.variable.indices")["sizes"] <= NCOL(data)))
             tmp.opacity <- 0.4
         else
             tmp.opacity <- 1.0
@@ -572,7 +572,7 @@ getPPTSettings <- function(chart.type, args, data)
 
     tmp.data.label.show <- isTRUE(args$data.label.show)
     tmp.data.label.show.category.labels <- FALSE
-    if (chart.type == "Scatter" && !isTRUE(args$scatter.labels.as.hovertext))
+    if (isScatter(chart.type) && !isTRUE(args$scatter.labels.as.hovertext))
         tmp.data.label.show <- TRUE
     if (chart.type %in% c("Donut", "Pie"))
     {
@@ -590,13 +590,14 @@ getPPTSettings <- function(chart.type, args, data)
         tmp.data.label.position <- "InsideEnd"
     else if (chart.type %in% c("Donut", "Pie"))
         tmp.data.label.position <- "OutsideEnd"
-    else if (chart.type == "Scatter")
+    else if (isScatter(chart.type))
         tmp.data.label.position <- "Center"
 
     # Behaviour of 'Automatically' set data label font colors
     # change depending on the chart type
     tmp.data.label.font.color <- ConvertCommaSeparatedStringToVector(args$data.label.font.color)
-    if (chart.type %in% c("Line", "Scatter") && isTRUE(args$data.label.font.autocolor))
+    if (chart.type %in% c("Line", "Scatter", "Bubble", "CombinedScatter") &&
+        isTRUE(args$data.label.font.autocolor))
         tmp.data.label.font.color <- args$colors
     else if (tmp.is.stacked && isTRUE(args$data.label.font.autocolor))
     {
@@ -616,7 +617,7 @@ getPPTSettings <- function(chart.type, args, data)
             ShowDataLabels = FALSE,
             BackgroundColor = args$density.color))
 
-    } else if (chart.type == "Scatter" && !isTRUE(args$scatter.colors.as.categorical))
+    } else if (isScatter(chart.type) && !isTRUE(args$scatter.colors.as.categorical))
     {
         # When scatterplots use colors as a numerical scale
         # we can assume a single template series
@@ -666,7 +667,7 @@ getPPTSettings <- function(chart.type, args, data)
     tmp.n <- length(series.settings)
 
 
-    if ((chart.type == "Scatter" && isTRUE(args$scatter.colors.as.categorical)) || chart.type == "Line")
+    if ((isScatter(chart.type) && isTRUE(args$scatter.colors.as.categorical)) || chart.type == "Line")
         for (i in 1:tmp.n)
             series.settings[[i]]$Marker = list(Size = args$marker.size,
                 OutlineStyle = "None",
@@ -774,7 +775,7 @@ getPPTSettings <- function(chart.type, args, data)
     if (chart.type == "Line")
         res$Smooth = isTRUE(args$shape == "Curved")
     if (chart.type %in% c("BarMultiColor", "ColumnMultiColor", "Pyramid", "Bar Pictograph") ||
-        (chart.type == "Scatter" && !isTRUE(args$scatter.colors.as.categorical)))
+        (isScatter(chart.type) && !isTRUE(args$scatter.colors.as.categorical)))
         res$ShowLegend <- FALSE
     if (chart.type == "Scatter" && tmp.data.label.show && isTRUE(args$grid.show))
     {
@@ -788,7 +789,7 @@ getPPTSettings <- function(chart.type, args, data)
 
     # There are some issues with Scatterplot exporting
     # See RS-7154 - try master.displayr.com
-    if (chart.type %in% c("Scatter"))
+    if (isScatter(chart.type))
     {
         res$ValueAxis$Crosses <- "Minimum"
         res$BubbleSizeType = if (isTRUE(args$scatter.sizes.as.diameter)) "Width" else "Area"
---
+++ tests/testthat/test-chartsettings-combinedscatter.R
@@ -0,0 +1,205 @@
+context("ChartSettings - CombinedScatter")
+
+dat <- LifeCycleSavings
+
+test_that("ChartSettings - CombinedScatter",
+{
+    v.ind <-  c(x = 1, y = 2, sizes = 3, colors = NA, groups = 5)
+    attr(dat, "scatter.variable.indices") <- v.ind
+    res <- CChart("CombinedScatter", dat, scatter.labels.as.hovertext = FALSE,
+            scatter.colors.column = NA, data.label.font.autocolor = TRUE,
+            scatter.sizes.as.diameter = FALSE, scatter.colors.as.categorical = TRUE,
+            values.grid.width = 0.5, values.grid.color = "#FF000080",
+            categories.grid.width = 0.5, categories.grid.color = "#FF000080",
+            colors = "#0000FF", marker.size = 6, opacity = 0.3, append.data = TRUE)
+    expect_equal(attr(res, "ChartSettings")$BubbleScale, 60)
+    expect_equal(attr(res, "ChartSettings")$BubbleSizeType, "Area")
+    expect_equal(attr(res, "ChartSettings")$TemplateSeries[[1]]$ShowDataLabels, TRUE)
+    expect_equal(attr(res, "ChartSettings")$TemplateSeries[[1]]$DataLabelsFont$color, "#0000FF")
+    expect_equal(attr(res, "ChartSettings")$TemplateSeries[[1]]$BackgroundColor, "#0000FF4C")
+    expect_equal(attr(res, "ChartSettings")$TemplateSeries[[1]]$Marker,
+            list(Size = 6, OutlineStyle = "None", BackgroundColor = "#0000FF4C"))
+    expect_equal(attr(res, "ChartSettings")$PrimaryAxis$MajorGridLine,
+            list(Color = "#FF000080", Width = 0.375009375234381, Style = "Solid"))
+    expect_equal(attr(res, "ChartSettings")$ValueAxis$MajorGridLine,
+            list(Color = "#FF000080", Width = 0.375009375234381, Style = "Solid"))
+    expect_equal(attr(res, "ChartLabels"), list(PrimaryAxisTitle = "sr", ValueAxisTitle = "pop15"))
+    expect_true(attr(res, "ChartSettings")$PrimaryAxis$ShowTitle)
+    expect_true(attr(res, "ChartSettings")$ValueAxis$ShowTitle)
+
+    v.ind <-  c(x = 1, y = 2, sizes = 3, colors = 4, groups = 5)
+    attr(dat, "scatter.variable.indices") <- v.ind
+    res <- CChart("CombinedScatter", dat, scatter.labels.as.hovertext = TRUE,
+            scatter.colors.column = NA, marker.size = 10,
+            scatter.sizes.as.diameter = TRUE, scatter.colors.as.categorical = TRUE,
+            colors = "#0000FF", append.data = TRUE)
+    expect_equal(attr(res, "ChartSettings")$BubbleScale, 100)
+    expect_equal(attr(res, "ChartSettings")$BubbleSizeType, "Width")
+    expect_equal(attr(res, "ChartSettings")$TemplateSeries[[1]]$ShowDataLabels, FALSE)
+    expect_equal(attr(res, "ChartSettings")$TemplateSeries[[1]]$BackgroundColor, "#0000FF66")
+    expect_equal(attr(res, "ChartSettings")$TemplateSeries[[1]]$Marker,
+            list(Size = 10, OutlineStyle = "None", BackgroundColor = "#0000FF66"))
+
+    res <- CChart("CombinedScatter", dat, scatter.labels.as.hovertext = TRUE,
+                  scatter.groups = round(dat[,5]/5), small.multiples = TRUE,
+                  warn.if.no.match = FALSE, append.data = TRUE)
+    expect_equal(attr(res, "ChartWarning"),
+                 " This visualization is a small multiple which is not supported by PowerPoint. It will be exported to PowerPoint as an image. Set 'PowerPoint Export > Format' to 'Microsoft Chart' and select a supported chart type or set the export format to 'Image' to suppress this warning.")
+
+    v.ind <-  c(x = 1, y = 2, sizes = NA, colors = 4, groups = 5)
+    attr(dat, "scatter.variable.indices") <- v.ind
+    res <- CChart("CombinedScatter", dat[1:3,], scatter.labels.as.hovertext = TRUE,
+            scatter.sizes.column = NA, scatter.colors.as.categorical = TRUE,
+            colors = c("#FF0000", "#00FF00", "#0000FF"), marker.size = 6,
+            append.data = TRUE)
+    expect_equal(attr(res, "ChartSettings")$TemplateSeries[[2]]$BackgroundColor, "#00FF00FF")
+    expect_equal(attr(res, "ChartSettings")$TemplateSeries[[3]]$Marker,
+            list(Size = 6, OutlineStyle = "None", BackgroundColor = "#0000FFFF"))
+
+    v.ind <-  c(x = 1, y = 2, sizes = 3, colors = 4, groups = 5)
+    attr(dat, "scatter.variable.indices") <- v.ind
+    res <- CChart("CombinedScatter", dat, scatter.colors.as.categorical = FALSE,
+          colors = c("red", "white", "blue"), append.data = TRUE)
+    expect_equal(attr(res, "ChartType"), "Bubble")
+    expect_equal(attr(res, "ChartSettings")$TemplateSeries[[1]]$OutlineStyle, "None")
+    expect_equal(attr(res, "ChartSettings")$TemplateSeries[[1]]$CustomPoints,
+            list(list(Index = 0, BackgroundColor = "#D9D9FF66", Marker = list(
+            BackgroundColor = "#D9D9FF66")), list(Index = 1, BackgroundColor = "#FFB8B866",
+            Marker = list(BackgroundColor = "#FFB8B866")), list(Index = 2,
+            BackgroundColor = "#F6F6FF66", Marker = list(BackgroundColor = "#F6F6FF66")),
+            list(Index = 3, BackgroundColor = "#FF0D0D66", Marker = list(
+            BackgroundColor = "#FF0D0D66")), list(Index = 4, BackgroundColor = "#FF535366",
+            Marker = list(BackgroundColor = "#FF535366")), list(Index = 5,
+            BackgroundColor = "#8484FF66", Marker = list(BackgroundColor = "#8484FF66")),
+            list(Index = 6, BackgroundColor = "#FF4A4A66", Marker = list(
+            BackgroundColor = "#FF4A4A66")), list(Index = 7, BackgroundColor = "#FF1A1A66",
+            Marker = list(BackgroundColor = "#FF1A1A66")), list(Index = 8,
+            BackgroundColor = "#FF181866", Marker = list(BackgroundColor = "#FF181866")),
+            list(Index = 9, BackgroundColor = "#FF313166", Marker = list(
+            BackgroundColor = "#FF313166")), list(Index = 10, BackgroundColor = "#C4C4FF66",
+            Marker = list(BackgroundColor = "#C4C4FF66")), list(Index = 11,
+            BackgroundColor = "#FF191966", Marker = list(BackgroundColor = "#FF191966")),
+            list(Index = 12, BackgroundColor = "#FFCFCF66", Marker = list(
+            BackgroundColor = "#FFCFCF66")), list(Index = 13, BackgroundColor = "#E9E9FF66",
+            Marker = list(BackgroundColor = "#E9E9FF66")), list(Index = 14,
+            BackgroundColor = "#C9C9FF66", Marker = list(BackgroundColor = "#C9C9FF66")),
+            list(Index = 15, BackgroundColor = "#FF656566", Marker = list(
+            BackgroundColor = "#FF656566")), list(Index = 16, BackgroundColor = "#FF1A1A66",
+            Marker = list(BackgroundColor = "#FF1A1A66")), list(Index = 17,
+            BackgroundColor = "#FF121266", Marker = list(BackgroundColor = "#FF121266")),
+            list(Index = 18, BackgroundColor = "#FFECEC66", Marker = list(
+            BackgroundColor = "#FFECEC66")), list(Index = 19, BackgroundColor = "#FF000066",
+            Marker = list(BackgroundColor = "#FF000066")), list(Index = 20,
+            BackgroundColor = "#FF888866", Marker = list(BackgroundColor = "#FF888866")),
+            list(Index = 21, BackgroundColor = "#FFA9A966", Marker = list(
+            BackgroundColor = "#FFA9A966")), list(Index = 22, BackgroundColor = "#FF989866",
+            Marker = list(BackgroundColor = "#FF989866")), list(Index = 23,
+            BackgroundColor = "#FF0F0F66", Marker = list(BackgroundColor = "#FF0F0F66")),
+            list(Index = 24, BackgroundColor = "#CACAFF66", Marker = list(
+            BackgroundColor = "#CACAFF66")), list(Index = 25, BackgroundColor = "#FF424266",
+            Marker = list(BackgroundColor = "#FF424266")), list(Index = 26,
+            BackgroundColor = "#E6E6FF66", Marker = list(BackgroundColor = "#E6E6FF66")),
+            list(Index = 27, BackgroundColor = "#FFD7D766", Marker = list(
+            BackgroundColor = "#FFD7D766")), list(Index = 28, BackgroundColor = "#FFB6B666",
+            Marker = list(BackgroundColor = "#FFB6B666")), list(Index = 29,
+            BackgroundColor = "#FF1E1E66", Marker = list(BackgroundColor = "#FF1E1E66")),
+            list(Index = 30, BackgroundColor = "#FF3E3E66", Marker = list(
+            BackgroundColor = "#FF3E3E66")), list(Index = 31, BackgroundColor = "#FF111166",
+            Marker = list(BackgroundColor = "#FF111166")), list(Index = 32,
+            BackgroundColor = "#FF282866", Marker = list(BackgroundColor = "#FF282866")),
+            list(Index = 33, BackgroundColor = "#FF080866", Marker = list(
+            BackgroundColor = "#FF080866")), list(Index = 34, BackgroundColor = "#FF3F3F66",
+            Marker = list(BackgroundColor = "#FF3F3F66")), list(Index = 35,
+            BackgroundColor = "#FF494966", Marker = list(BackgroundColor = "#FF494966")),
+            list(Index = 36, BackgroundColor = "#FF151566", Marker = list(
+            BackgroundColor = "#FF151566")), list(Index = 37, BackgroundColor = "#FF585866",
+            Marker = list(BackgroundColor = "#FF585866")), list(Index = 38,
+            BackgroundColor = "#5B5BFF66", Marker = list(BackgroundColor = "#5B5BFF66")),
+            list(Index = 39, BackgroundColor = "#B2B2FF66", Marker = list(
+            BackgroundColor = "#B2B2FF66")), list(Index = 40, BackgroundColor = "#FF272766",
+            Marker = list(BackgroundColor = "#FF272766")), list(Index = 41,
+            BackgroundColor = "#FF141466", Marker = list(BackgroundColor = "#FF141466")),
+            list(Index = 42, BackgroundColor = "#FFE0E066", Marker = list(
+            BackgroundColor = "#FFE0E066")), list(Index = 43, BackgroundColor = "#0000FF66",
+            Marker = list(BackgroundColor = "#0000FF66")), list(Index = 44,
+            BackgroundColor = "#FF5E5E66", Marker = list(BackgroundColor = "#FF5E5E66")),
+            list(Index = 45, BackgroundColor = "#FF060666", Marker = list(
+            BackgroundColor = "#FF060666")), list(Index = 46, BackgroundColor = "#FF252566",
+            Marker = list(BackgroundColor = "#FF252566")), list(Index = 47,
+            BackgroundColor = "#FF585866", Marker = list(BackgroundColor = "#FF585866")),
+            list(Index = 48, BackgroundColor = "#FF040466", Marker = list(
+            BackgroundColor = "#FF040466")), list(Index = 49, BackgroundColor = "#FF141466",
+            Marker = list(BackgroundColor = "#FF141466"))))
+
+    pasted <- list(structure(c("", "a", "b", "c", "d", "e", "f", "g", "x",
+            "1", "2", "4", "2", "4", "6", "3", "y", "6", "7", "5", "5", "2",
+            "8", "9", "z", "1", "2", "3", "4", "5", "6", "7"), .Dim = c(8L,
+            4L)), FALSE, NULL, NULL)
+    pd <- PrepareData("CombinedScatter", input.data.pasted = pasted)
+    res <- CChart("CombinedScatter", pd$data, colors = "#FF0000", opacity = NULL,
+                  append.data = TRUE, scatter.colors.as.categorical = TRUE)
+    expect_equal(attr(res, "ChartSettings")$TemplateSeries[[1]]$BackgroundColor, "#FF000066")
+
+})
+
+
+dat.char.colors <- structure(list(`X-coord` = c(2, 6, 7, 3, 4), `Y-coord` = c(3,
+5, 7, 3, 8), Size = c(1, 2, 3, 4, 5), Group = c("A", "A", "B",
+"B", "C")), row.names = c("alpha", "beta", "gamma", "delta",
+"epsilon"), scatter.variable.indices = c(x = 1, y = 2, sizes = 3,
+colors = 4, groups = 4), class = "data.frame")
+
+dat.char.coords <- structure(list(Day = c("Monday", "Tuesday", "Monday", "Wednesay",
+"Friday", "Monday", "Tuesday", "Thursday", "Wednesday"), Time = c("Lunch",
+"Dinner", "Breakfast", "Breakfast", "Lunch", "Lunch", "Dinner",
+"Lunch", "Lunch")), row.names = c("Amy", "Ben", "Jim", "Tim",
+"Ian", "Sara", "Rod", "Bob", "Liz"), scatter.variable.indices = c(x = 1,
+y = 2, sizes = NA, colors = NA, groups = 2), class = "data.frame")
+
+dat.factor.coords <- structure(list(Species = structure(c(2L, 2L, 2L, 1L, 3L, 2L,
+3L, 3L, 2L, 2L, 2L, 2L, 3L, 2L, 2L, 3L, 3L, 3L, 1L, 1L), .Label = c("setosa",
+"versicolor", "virginica"), class = "factor"), `Grid: Petal.Length` = c(4.7,
+4.4, 4.1, 1.7, 5.1, 3.7, 4.8, 6.4, 4.4, 4.7, 4.1, 4.5, 5.1, 4.5,
+4.1, 6.7, 5.4, 5.7, 1.6, 1.4)), scatter.variable.indices = c(x = 1,
+y = 2, sizes = NA, colors = NA, groups = NA), row.names = c(64L,
+91L, 68L, 24L, 111L, 82L, 139L, 132L, 88L, 51L, 89L, 69L, 134L,
+85L, 100L, 123L, 140L, 125L, 26L, 18L), class = "data.frame")
+
+test_that("CombinedScatterplot ChartData conversion",
+{
+    res <- convertChartDataToNumeric(dat.char.colors)
+    expect_equal(unlist(lapply(res, class)), c(`X-coord` = "numeric",
+            `Y-coord` = "numeric", Size = "numeric", Group = "factor"))
+    expect_equal(dim(res), dim(dat.char.colors))
+    expect_equal(attr(res, "scatter.variable.indices"),
+            attr(dat.char.colors, "scatter.variable.indices"))
+
+    res <- convertChartDataToNumeric(dat.char.coords)
+    expect_equal(unlist(lapply(res, class)), c(Day = "integer", Time = "integer"))
+    expect_equal(dim(res), dim(dat.char.coords))
+    expect_equal(attr(res, "scatter.variable.indices"),
+            attr(dat.char.coords, "scatter.variable.indices"))
+
+    res <- convertChartDataToNumeric(dat.factor.coords)
+    expect_equal(unlist(lapply(res, class)),
+            c(Species = "integer", `Grid: Petal.Length` = "numeric"))
+    expect_equal(dim(res), dim(dat.factor.coords))
+    expect_equal(attr(res, "scatter.variable.indices"),
+            attr(dat.factor.coords, "scatter.variable.indices"))
+})
+
+dat <- structure(list(multi1 = structure(c(6L, 5L, 3L, 3L, 3L, 2L, 4L,
+2L, 9L, 2L, 6L, 4L, 7L, 6L, 10L, 7L, 6L, 2L, 3L, 5L), .Dim = c(10L,
+2L), .Dimnames = list(c("A", "B", "C", "D", "E", "F", "G", "H",
+"I", "J"), c("Col 1", "Col 2"))), multi2 = structure(c(7, 6,
+4, 4, 4, 3, 5, 3, 10, 3, 6, 4, 7, 6, 10, 7, 6, 2, 3, 5), .Dim = c(10L,
+2L), .Dimnames = list(c("A", "B", "C", "D", "E", "F", "G", "H",
+"I", "J"), c("Col 1", "Col 2"))), multi3 = structure(c(7, 6,
+4, 4, 4, 3, 5, 3, 10, 3, 7, 5, 8, 7, 11, 8, 7, 3, 4, 6), .Dim = c(10L,
+2L), .Dimnames = list(c("A", "B", "C", "D", "E", "F", "G", "H",
+"I", "J"), c("Col 1", "Col 2")))), scatter.variable.indices = c(x = 1,
+y = 2, sizes = 3, colors = 4))
+test_that("CombinedScatter with multiple tables",
+{
+    expect_warning(CChart("CombinedScatter", dat, append.data = TRUE))
+})
