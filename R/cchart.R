9bc96562587837231a1eb4c4f9c23eb66e04fcf6
chschan <chschan@users.noreply.github.com>
2024-07-03 15:07:39 +1000

    Make GapWidth depend on both bar.gap and bar.group.gap

--- R/cchart.R
+++ R/cchart.R
@@ -772,8 +772,9 @@ getPPTSettings <- function(chart.type, args, data)
     {
         if (!tmp.is.stacked && NCOL(data) > 1 && !is.null(args$bar.group.gap))
         {
-            res$Overlap = min(1.0, args$bar.group.gap / (1 - args$bar.group.gap)) * -100
-            res$GapWidth = (min(5.0, args$bar.gap / (1 - args$bar.gap)) * 100) + 100
+            tmp.gap = min(1.0, args$bar.group.gap / (1 - args$bar.group.gap))
+            res$Overlap = tmp.gap * -100
+            res$GapWidth = min(5.0, args$bar.gap / (1 - args$bar.gap) + tmp.gap) * 100
         } else
             res$GapWidth = min(5.0, args$bar.gap / (1 - args$bar.gap)) * 100 
     }
