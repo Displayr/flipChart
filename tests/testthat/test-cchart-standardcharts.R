2fdb0f0f140cdc8227d7371a99fdc63855a0fbc6
chschan <carmen.chan@displayr.com>
2024-09-09 16:24:32 +1000

    Undo fix to test

--- tests/testthat/test-cchart-standardcharts.R
+++ tests/testthat/test-cchart-standardcharts.R
@@ -111,7 +111,7 @@ for (input in list( RawData.XFactor,  RawData.XFactor.YFactor, RawData.XPickAny,
      }
 
 # Raw data inputs - first aggregate = TRUE
-for (input in list( RawData.XFactor,  #RawData.XFactor.YFactor,
+for (input in list( RawData.XFactor,  RawData.XFactor.YFactor,
                     RawData.XPickAny, RawData.XPickOneMulti, RawData.XNumberMulti))
     for (chart.type in c("Line", "Bar", "Area", "Column"))
     {
@@ -119,9 +119,6 @@ for (input in list( RawData.XFactor,  #RawData.XFactor.YFactor,
         c <- CChart(chart.type, pd$data, y.zero = FALSE, y.zero.line.width = 1,  grid.show = FALSE, y.title = "Dog")
         expect_error(print(c), NA)
     }
-    pd <- suppressWarnings(PrepareData(chart.type, input.data.raw = RawData.XFactor.YFactor, first.aggregate = TRUE))
-    expect_warning(c <- CChart("Column", pd$data, y.zero = FALSE,
-            y.zero.line.width = 1,  grid.show = FALSE, y.title = "Dog",), "Missing values")
 
 # Raw data inputs - first aggregate = TRUE, as.percentages = TRUE
 for (input in list(RawData.XFactor, RawData.XPickAny, RawData.XPickOneMulti, RawData.XNumberMulti))
